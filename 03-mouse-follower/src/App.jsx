2ea0724a1d253dafc28fa7127ced39c6b30b7fea
jonatan <jonalaual_2000@hotmail.com>
2024-09-18 12:57:49 -0500

    actaulizando dev

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -22,11 +22,15 @@ const FollowMouse = () => {
     // => cunado el componente se demora
     // =>cuando cambia las dependencias, antes de ejecutar
     // el efecto de nuevo
-    return () => {
+    return () => { //cleanup method
      console.log('cleanup')
       window.removeEventListener('pointermove', handleMove)
     }
   }, [enabled])
+  
+  //[] -> solo se ejecuta una vez cuando se monta el componente
+  //[enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+  //undefined -> se ejecuta cada vez que se renderiza el componente
 
   //change body className
   useEffect(() =>{
